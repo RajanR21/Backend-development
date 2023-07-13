876b5eb5d601ca2078a86f2b59c4f8c86c49820a
Rajan Ravisaheb <rajanroy220@gmail.com>
2023-07-13 15:36:07 +0530

    added mongodb database

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from "express";
 import path from 'path';
+import mongoose from "mongoose";
+
+//how to connect to mongodb
+mongoose.connect("mongodb://127.0.0.1:27017" , {
+    dbname : "backend",
+}).then(c => console.log("connected")).catch(e => console.log("error"))
+
+//created a schema
+const message_schema = new mongoose.Schema({
+    name : String , 
+    email : String ,
+})
+
+//created model
+const message = mongoose.model("Message" , message_schema);
 
 const app = express()
 app.set('view engine','ejs'); 
 
 let users = [];
+
 //using middlewares
 //app.use(express.static(path.join(path.resolve() , "public")));
 app.use(express.urlencoded({extended : true}));
@@ -14,13 +30,14 @@ app.get("/" , (req , res) => {
   //  res.sendFile("index.html");
 })
 
+
 app.get("/users" , (req , res) => {
     res.send({
         users,
     })
 })
-app.post("/" , (req , res) => {
-    users.push({
+app.post("/" , async(req , res) => {
+    await message.create({
         "name" : req.body.name,
         "email" : req.body.email,
     })
